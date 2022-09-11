f4bab215717426ccabdde91003c372ea76ae34e4
manuelJung <manuel.jung.wwi12@gmail.com>
2022-09-11 23:25:55 +0200

    bugfixed

--- netlify/edge-functions/rewrite-url.ts
+++ netlify/edge-functions/rewrite-url.ts
@@ -3,6 +3,8 @@
 // http://localhost:8888 -> http://localhost:8888/content-b
 export default async (request: Request, context: any) => {
   let [,host, pathname] = request.url.match(/(https?:\/\/[^\/]*)(.*)/)!
+
+  if(pathname.includes('/content-b')) return
   
   // /script.js -> /content-b/script.js
   const bUrl = request.url.replace(host, '/content-b' + pathname)
